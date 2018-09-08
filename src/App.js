89cfb40af1d17ffb9eb487226003cc44050ca994
oskojess <jessica_osko@hotmail.com>
2018-09-08 10:56:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+     <div>
+       <p></p>
+     </div>
+    );
+  }
+}
+
+export default App;
