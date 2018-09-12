d5b4b9715c1a438090b194fa031455f124f0d43d
sasasancei <sasasancei@hotmail.com>
2018-09-11 21:02:04 -0300

    Teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './components/template/dependencies'
+import "./components/template/dependencies";
 
-import React, { Component } from 'react';
-import SideBar from './components/template/sideBar'
+import React, { Component } from "react";
+import SideBar from "./components/template/sideBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-		<SideBar />
-		<div className="content-wrapper">
-		</div>
+        <SideBar />
+        <div className="content-wrapper" />
+        <div className="isso e so um teste" />
       </div>
     );
   }
