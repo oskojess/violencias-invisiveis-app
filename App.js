6666db49eaf674549912c496bfb9cf58cda346de
Amynata Nepomuceno <41297400+amynatan@users.noreply.github.com>
2018-09-11 21:10:07 -0300

    teste (#6)
    * teste
    
    * vamo vê

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>First teste - Webpack</p>
+        <p>T-E-S-T-E</p>
+        <p>bereguei</p>
       </div>
     );
   }
