22cc2ada3ab89e8df04f5459175dec9c7ac151de
oskojess <jessica_osko@hotmail.com>
2018-10-03 08:34:25 -0300

    add color in appbar

--- src/components/template/AppBar.js
+++ src/components/template/AppBar.js
@@ -6,34 +6,52 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { withRouter } from "react-router";
+import PropTypes from "prop-types";
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  appBar: {
+    backgroundColor: "#9E002D"
+  }
+});
 
 class AppBar extends Component {
   render() {
     const { classes, anchor, open } = this.props;
 
     return (
-      <AppBarComponent
-        className={classNames(classes.appBar, {
-          [classes.appBarShift]: open,
-          [classes[`appBarShift-${anchor}`]]: open
-        })}
-      >
-        <Toolbar disableGutters={!open}>
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            onClick={() => this.props.handleDrawerOpen()}
-            className={classNames(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" noWrap>
-            Violências Invisíveis
-          </Typography>
-        </Toolbar>
-      </AppBarComponent>
+      <MuiThemeProvider theme={theme}>
+        <AppBarComponent
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+            [classes[`appBarShift-${anchor}`]]: open
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={() => this.props.handleDrawerOpen()}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Violências Invisíveis
+            </Typography>
+          </Toolbar>
+        </AppBarComponent>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default withRouter(AppBar);
+AppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRouter(withStyles(theme)(AppBar));
