5c6c40e1175db0d8ae1cafb1f8c2607a8aabad23
Amynata Nepomuceno <41297400+amynatan@users.noreply.github.com>
2018-10-03 10:06:04 -0300

    create new petitions card (#46)

--- src/components/admin/components/card/Petitions.js
+++ src/components/admin/components/card/Petitions.js
@@ -1,10 +1,43 @@
 import * as React from "react";
 import { withRouter } from "react-router";
+import Divider from "@material-ui/core/Divider";
+import MainTitle from "../../../template/MainTitle";
+import PetitionCard from "../../../template/PetitionCard";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
-class Petitions extends React.PureComponent {
+const styles = () => ({
+  titleDivider: {
+    height: "3px",
+    backgroundColor: "#1ea896"
+  },
+  space: {
+    height: "25px"
+  }
+});
+
+class Petitions extends React.Component {
   render() {
-    return <p>Petitions</p>;
+    const { classes } = this.props;
+    return (
+      <div>
+        <MainTitle content="PETIÇÕES" icon="access_time" />
+        <Divider className={classes.titleDivider} />
+        <div className={classes.space} />
+
+        <PetitionCard
+          status={1}
+          protocol={"COD918375"}
+          description={"Lorem ipsum dolor sit amet, consectetuer adipiscing."}
+          observations={"Lorem ipsum dolor sit amet, consectetuer adipiscing."}
+        />
+      </div>
+    );
   }
 }
 
-export default withRouter(Petitions);
+Petitions.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRouter(withStyles(styles)(Petitions));
