e0522a6f448aeb0034802a8c818799ee9dcb6143
oskojess <oskojess@gmail.com>
2018-12-07 13:47:33 -0200

    ajustes tutorial

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,16 @@
 import * as React from "react";
 import { withRouter } from "react-router";
 import Divider from "@material-ui/core/Divider";
-import Icon from "@material-ui/core/Icon";
-import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import MainTitleDashboard from "../components/template/MainTitleDashboard";
 import MainButton from "./template/MainButton";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import audio from "../utils/audioConfig";
-import { connect } from "react-redux";
+import Icon from "@material-ui/core/Icon";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const styles = () => ({
   space: {
@@ -53,24 +52,23 @@ const styles = () => ({
   cardDivider: {
     width: "90%",
     textAlign: "center",
+    justifyContent: "center",
     margin: "0 auto",
-    backgroundColor: "#fff"
+    backgroundColor: "#CDCDC1"
   },
   startPetitionButton: {
     border: "2px solid red",
     borderRadius: "3px"
   },
-
-  spaceDivider: {
-    width: "80%",
-    margin: "0 auto",
-    textAlign: "center",
-    backgroundColor: "#CDCDC1",
-  },
   labelDashboard: {
-    
     fontFamily: "Helvetica Neue,Helvetica,Arial,Tahoma,sans-serif",
-    fontSize: "18px",
+    fontSize: "18px"
+  },
+  styleFont: {
+    fontFamily: "Helvetica Neue,Helvetica,Arial,Tahoma,sans-serif"
+  },
+  styleText:{
+    color: "#CDCDC1",
   }
 });
 
@@ -94,7 +92,7 @@ class Dashboard extends React.Component {
   render() {
     const { classes, history } = this.props;
     return (
-      <div>
+      <div className={classes.styleFont}>
         <MainTitleDashboard content="Violências Invisíveis" />
 
         <div className={classes.space} />
@@ -122,22 +120,49 @@ class Dashboard extends React.Component {
           />
         </CardActions>
         <div className={classes.labelDashboard}>
-          <p><b>Este é o aplicativo para quebrar o silêncio.</b></p>
+          <p>
+            <b>Este é o aplicativo para quebrar o silêncio.</b>
+          </p>
           <p>
             Bem-vinda! O botão acima te ajuda a criar uma petição. Assim, você
             pode expor seu problema, pedir ajuda e conseguir apoio. Sabemos que
             é um passo difícil, mas te encorajamos a tentar!
           </p>
           <p>
-            <b>Use como se fosse o zap.</b> Escreva ou mande áudio para responder as
-            perguntas. Em poucos passos você terá sua petição.
+            <b>Use como se fosse o zap.</b> Escreva ou mande áudio para
+            responder as perguntas. Em poucos passos você terá sua petição.
           </p>
         </div>
         <div className={classes.space} />
-        <div className={classes.spaceDivider} />
- 
-   
-    </div>
+        <Divider className={classes.cardDivider} />
+        <ListItem
+          button
+          onClick={() =>
+            this.handleChangePage("follow", "Acompanhar o seu abaixo-assinado")}
+      style={{  justifyContent: "center"}}
+          aria-label="Acompanhar abaixo-assinado"
+        >
+          <Icon className={classes.iconMenu}>schedule</Icon>
+          <ListItemText className={classes.fontStyle} primary="Acompanhar" />
+        </ListItem>
+
+        <Divider className={classes.cardDivider} />
+        <ListItem
+          button
+          onClick={() => this.handleChangePage("tutorial", "Assistir tutorial")}
+          style={{  textAlign: "center"}}
+          aria-label="Assistir Tutorial"
+        >
+          <Icon className={classes.iconMenu}>play_circle_outline</Icon>
+          <ListItemText
+            className={classes.fontStyle}
+            primary="Assistir Tutorial"
+          />
+        </ListItem>
+
+ <Divider className={classes.cardDivider} />
+        <p className={classes.styleText}>Este app é uma parceria Change.org Brasil e Minas Programam (Saiba mais)</p>
+</div>
     );
   }
 }
--- src/components/user/components/tutorial/TutorialCard.js
+++ src/components/user/components/tutorial/TutorialCard.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { withRouter } from "react-router";
-import Divider from '@material-ui/core/Divider';
-import MainTitle from '../../../template/MainTitle';
-import SimpleDialog from '../../../template/Dialog';
+import Divider from "@material-ui/core/Divider";
+import MainTitle from "../../../template/MainTitle";
+import SimpleDialog from "../../../template/Dialog";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
@@ -25,20 +25,32 @@ class TutorialCard extends React.Component {
     };
   }
 
-  componentDidMount(){
-    document.title = "Violências Invisíveis - Tutorial"
+  componentDidMount() {
+    document.title = "Violências Invisíveis - Tutorial";
   }
 
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <MainTitle content="TUTORIAL" icon="play_arrow"></MainTitle>
-        
-        <div className={classes.space} aria-hidden="true"></div>
-        <iframe aria-label="Video Tutorial" width="100%" height="315" src="https://www.youtube.com/embed/39CprM640f8" frameBorder="0" allowFullScreen></iframe>
-     </div>
+        <div>
+          <MainTitle content="TUTORIAL" icon="play_circle_outline" />
 
+          <div className={classes.space} aria-hidden="true" />
+          <iframe
+            aria-label="Video Tutorial"
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/39CprM640f8"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+        <div className={classes.space} />
+        <div>
+          <iframe src="http://www.comousarachange.org/" style={{border:"none"}}  width="100%" />
+        </div>
+      </div>
     );
   }
 }
